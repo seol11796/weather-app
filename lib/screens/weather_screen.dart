8cd9e325db5c1aded33aa11b83dea58f2572b082
seol <seol11796@gmail.com>
2022-03-14 14:51:59 +0900

    third commit

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,8 +1,8 @@
 
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WeatherScreen extends StatefulWidget {
   WeatherScreen({this.parseWeatherData});
@@ -41,30 +41,56 @@ class _WeatherScreenState extends State<WeatherScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                '$cityName',
-                style: TextStyle(
-                    fontSize: 30.0
-                ),
+      extendBodyBehindAppBar:true ,
+     appBar: AppBar(
+      //title:Text(''),
+       backgroundColor: Colors.transparent,
+       elevation: 0.0,
+       leading: IconButton(
+         icon: Icon(Icons.near_me),
+         onPressed: (){},
+         iconSize: 30.0,
+       ),
+       actions: [
+         IconButton(
+            icon: Icon(
+              Icons.location_searching,
+            ),
+           onPressed: (){},
+           iconSize: 30.0,
+         )
+       ],
+     ),
+      body:Container(
+        child: Stack(
+          children:
+          [
+            Image.asset('image/background.jpg',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+            ),
+            Container(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 150.0,
+                  ),
+                  Text(
+                    'Seoul',
+                    style: GoogleFonts.lato(
+                      fontSize:35.0,
+                      fontWeight: FontWeight.bold,
+                      color:Colors.white
+                    ),
+                  )
+                ],
               ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                '$temp',
-                style: TextStyle(
-                    fontSize: 30.0
-                ),
-              ),
-            ],
-          ),
+            )
+          ],
         ),
-      ),
+      )
     );
   }
 }
